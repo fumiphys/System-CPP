f44b69fa4b319158a22d968e52d56d32dfe73807
fumiphys <ufimihot2703@gmail.com>
2018-11-19 01:47:47 +0900

    Compiler: implement calculating expression

--- Compiler/fcc.c
+++ Compiler/fcc.c
@@ -7,12 +7,25 @@
 
 int main(int argc, char const* argv[])
 {
-  int ret;
-  scanf("%d", &ret);
+  char exp[256];
+  scanf("%s", exp);
+  char *s = exp;
   printf(".intel_syntax_noprefix\n");
   printf(".global _main\n");
   printf("_main:\n");
-  printf("  mov rax, %d\n", ret);
+  printf("  mov rax, %ld\n", strtol(s, &s, 10));
+  while(*s){
+    if(*s == '+'){
+      s++;
+      printf("  add rax, %ld\n", strtol(s, &s, 10));
+    }else if(*s == '-'){
+      s++;
+      printf("  sub rax, %ld\n", strtol(s, &s, 10));
+    }else{
+      fprintf(stderr, "undefined char sequence\n");
+      return 1;
+    }
+  }
   printf("  ret\n");
   return 0;
 }
